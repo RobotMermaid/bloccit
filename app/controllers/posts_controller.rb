c99234f210fa4921f819e189d1aa7f3bba6d7bba
RobotMermaid <ondinerangel@gmail.com>
2016-11-29 14:59:56 -0800

    post title to spam

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    #if post is fifth change post.title to 'spam'
+    @posts.each_with_index do |post, index|
+      if index % 5 == 0
+        post.title = "SPAM"
+      end
+    end
   end
 
   def show
@@ -10,5 +16,6 @@ class PostsController < ApplicationController
   end
 
   def edit
+
   end
 end
