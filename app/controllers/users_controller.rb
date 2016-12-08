bdd4d1bf8387dde85e67df124d1c9900c2e4b2da
RobotMermaid <ondinerangel@gmail.com>
2016-12-08 11:37:06 -0800

    confirmation page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,4 +20,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def confirm
+    @user = User.new
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+  end
+
 end
