f6734c354451cb79669d75a35e422580d23cc7bc
RobotMermaid <ondinerangel@gmail.com>
2016-12-09 10:40:34 -0800

    Associations and Mass Assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,26 @@
 class PostsController < ApplicationController
 
+  before_action :require_sign_in, except: :show
 
   def show
     @post = Post.find(params[:id])
   end
+
 
   def new
     @topic = Topic.find(params[:topic_id])
     @post = Post.new
   end
 
+
   def create
-    @post = Post.new
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
+    #@post = Post.new
+    #@post.title = params[:post][:title]
+    #@post.body = params[:post][:body]
+    #@post.topic = @topic
     @topic = Topic.find(params[:topic_id])
-    @post.topic = @topic
+    @post = @topic.posts.build(post_params)
+    @post.user = current_user
 
     if @post.save
       flash[:notice] = "Post was saved."
@@ -26,14 +31,17 @@ class PostsController < ApplicationController
     end
   end
 
+
   def edit
     @post = Post.find(params[:id])
   end
 
+
   def update
     @post = Post.find(params[:id])
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
+    #@post.title = params[:post][:title]
+    #@post.body = params[:post][:body]
+    @post.assign_attributes(post_params)
 
     if @post.save
       flash[:notice] = "Post was updated."
@@ -44,6 +52,7 @@ class PostsController < ApplicationController
     end
   end
 
+
   def destroy
     @post = Post.find(params[:id])
     if @post.destroy
@@ -53,5 +62,10 @@ class PostsController < ApplicationController
       flash[:alert] = "There was an error deleting the post"
       render :show
     end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
