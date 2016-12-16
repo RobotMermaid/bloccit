9c42ecc8d23bc268fde65e249604074901738b6b
RobotMermaid <ondinerangel@gmail.com>
2016-12-15 16:55:31 -0800

    Profiles

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
-  let(:name) { RandomData.random_sentence }
-  let(:description) { RandomData.random_paragraph }
   let(:public) { true }
-  let(:topic) { Topic.create!(name: name, description: description) }
+  let(:topic) { create(:topic) }
 
   describe "attributes" do
     it "has name, description, and public attributes" do
-      expect(topic).to have_attributes(name: name, description: description, public: public)
+      expect(topic).to have_attributes(name: topic.name, description: topic.description)
     end
 
     it "is public by default" do
       expect(public).to be(true)
     end
 
-    it { is_expected.to have_many(:posts) }  
+
+
+    it { is_expected.to have_many(:posts) }
   end
 end
